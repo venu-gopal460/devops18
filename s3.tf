30b157bd54920fcfc329589b400ff639c31f0f56
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2024-10-16 19:30:28 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafaccit.devops.project.bucket"
+  bucket = "mustafaflm.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -23,10 +23,4 @@ status = "Enabled"
 }
 }
 
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafaccit.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
-}
+
