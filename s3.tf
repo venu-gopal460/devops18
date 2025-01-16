152657cf87a4cb978511305dbaf06457939fbd85
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2025-01-16 21:24:37 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik.devops.project.bucket"
+  bucket = "mustafashaik77.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
