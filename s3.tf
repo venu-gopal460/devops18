17102117bdea37603b2324260370a48f4e36b070
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2025-01-16 21:26:58 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,11 +22,3 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafashaik.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
-}
