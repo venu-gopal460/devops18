e4b95394a88f711dfeb0c4d7af484f0dcd3940c7
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2025-01-16 21:23:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0ddc798b3f1a5117e"
+    image_id           = "ami-0454e52560c7f5c55"
     instance_type = "t2.micro"
-    key_name = "mustafakey-pair"
+    key_name = "lastone"
     
 }
    
@@ -11,7 +11,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0883b34c988bcbf20", "subnet-0f4f4322d734fcc38"]
+     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
