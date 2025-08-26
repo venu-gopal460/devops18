af6d2097d6eef9f0aaace15e77c4b0e19dfaeca5
venu-gopal460 <venugopaljala460@gmail.com>
2025-08-26 11:12:57 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,10 +22,4 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafashaik77.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
-}
+
